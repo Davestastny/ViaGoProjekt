368a1bcfc0fa77fea139af0b3f371b0c7479c506
Daves <davestastny@email.cz>
2024-02-25 20:08:15 +0100

    new page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import {Routes} from '@angular/router';
 import {IndexComponent} from "./pages/index/index.component";
+import {ModelsComponent} from "./pages/models/models.component";
 
 export const routes: Routes = [
   {path: '', component: IndexComponent},
+  {path: 'models', component: ModelsComponent},
 ];
