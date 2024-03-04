f883286b01341164eb1705ec0740f3f1f9655315
Davestastny <davestastny@email.cz>
2024-03-04 14:14:40 +0100

    product cards upgrade, license page

--- src/app/pages/license/license.component.ts
+++ src/app/pages/license/license.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {ProductCardComponent} from "../../components/product-card/product-card.component";
 
 @Component({
   selector: 'app-license',
   standalone: true,
-  imports: [],
+    imports: [
+        ProductCardComponent
+    ],
   templateUrl: './license.component.html',
   styleUrl: './license.component.css'
 })
