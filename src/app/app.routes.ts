723d5b8d7621a2f58d7d87643594e70a16eb6587
Davestastny <davestastny@email.cz>
2024-03-07 20:20:09 +0100

    frontend pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import {IndexComponent} from "./pages/index/index.component";
 import {ModelsComponent} from "./pages/models/models.component";
 import {NolicenseComponent} from "./pages/nolicense/nolicense.component";
 import {LicenseComponent} from "./pages/license/license.component";
+import {ContactComponent} from "./pages/contact/contact.component";
 
 export const routes: Routes = [
   {path: '', component: IndexComponent},
   {path: 'models', component: ModelsComponent},
   {path: 'nolicense', component: NolicenseComponent},
   {path: 'license', component: LicenseComponent},
+  {path: 'contact', component: ContactComponent},
 
 ];
