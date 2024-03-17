a434caa6847a7a5926872805253c320345a8b346
Davestastny <davestastny@email.cz>
2024-03-17 22:39:07 +0100

    icense backend

--- src/app/pages/license/license.component.ts
+++ src/app/pages/license/license.component.ts
@@ -1,15 +1,31 @@
-import { Component } from '@angular/core';
-import {ProductCardComponent} from "../../components/product-card/product-card.component";
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Product } from '../../models/product.model';
+import { ProductDataService } from '../../services/product-data.service';
+import { ProductCardComponent } from "../../components/product-card/product-card.component";
+import { AsyncPipe, NgForOf, CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-license',
   standalone: true,
-    imports: [
-        ProductCardComponent
-    ],
+  imports: [
+    ProductCardComponent,
+    AsyncPipe,
+    NgForOf,
+    CommonModule
+  ],
   templateUrl: './license.component.html',
-  styleUrl: './license.component.css'
+  styleUrls: ['./license.component.css']
 })
-export class LicenseComponent {
+export class LicenseComponent implements OnInit {
+  licenseProducts$!: Observable<Product[]>; // Produkty, které vyžadují řidičák
 
+  constructor(private productService: ProductDataService) {}
+
+  ngOnInit() {
+    this.licenseProducts$ = this.productService.getProducts().pipe(
+      map(products => products.filter(product => product.slug !== 'elektroskutr-viago-riga' && product.slug !== 'elektroskutr-viago-tokio'))
+    );
+  }
 }
