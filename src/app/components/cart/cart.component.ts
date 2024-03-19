e05f56440e6ad627530da2dda5c52f9f83197595
Davestastny <davestastny@email.cz>
2024-03-19 20:20:36 +0100

    shopping cart backend

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ShoppingCartService } from '../../services/shopping-cart.service';
 import { CartItem } from '../../models/cart-item.model';
-import {CurrencyPipe} from "@angular/common"; // Cesta k souboru modelu CartItem
+import {CurrencyPipe, NgForOf, NgIf} from "@angular/common"; // Cesta k souboru modelu CartItem
 
 
 
@@ -10,12 +10,14 @@ import {CurrencyPipe} from "@angular/common"; // Cesta k souboru modelu CartItem
   templateUrl: './cart.component.html',
   standalone: true,
   imports: [
-    CurrencyPipe
+    CurrencyPipe,
+    NgIf,
+    NgForOf
   ],
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
-  cartItems: CartItem[] = []; // Explicitně určuje typ jako CartItem[]
+  cartItems: CartItem[] = [];
 
   constructor(private shoppingCartService: ShoppingCartService) {}
 
