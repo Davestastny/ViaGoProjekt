813370e3be99e1505c11920d98d2ad3898a60442
Davestastny <davestastny@email.cz>
2024-03-22 10:32:51 +0100

    responsivity pages

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,16 +1,22 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  mobileNavOpen = false;
+
   constructor(private router: Router) {}
+
+  toggleNavbarLinks(): void {
+    this.mobileNavOpen = !this.mobileNavOpen;
+  }
 
   goToCart(): void {
     this.router.navigate(['/cart']);
